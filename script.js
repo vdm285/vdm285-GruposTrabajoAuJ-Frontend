678c43e595bd95ecad52d2ad47644f37f2536440
vdm285 <victor.d.mikhailov@gmail.com>
2024-08-14 01:44:17 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const greetButton = document.getElementById('greet-btn');
+    const colorButton = document.getElementById('color-btn');
+    
+    greetButton.addEventListener('click', function () {
+        alert('¡A la espera de la próxima misión!');
+    });
+    
+    colorButton.addEventListener('click', function () {
+        document.body.classList.toggle('toggled-style');
+    });
+});
